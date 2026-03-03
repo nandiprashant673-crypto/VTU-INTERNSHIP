098d6e18d52f1414a94afb8be43caca6fb5d65f5
nandiprashant673-crypto <nandiprashant673@gmail.com>
2026-03-03 20:12:24 +0530

    Delete WEEK 4/Week4/FullStack-Hosting-Project/frontend/frontend/script.js

--- WEEK 4/Week4/FullStack-Hosting-Project/frontend/frontend/script.js
+++
@@ -1,7 +0,0 @@
-function getMessage() {
-    fetch("https://YOUR_RENDER_BACKEND_URL/api/message")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("output").innerText = data.message;
-    });
-}
